63af3530cba02b777da6520ab0e116c050fbe507
qmlearning <116808999+qmlearning@users.noreply.github.com>
2022-10-27 22:16:53 +1030

    Create overview.js

---
+++ data/overview.js
@@ -0,0 +1,15 @@
+var luvstep_data = {
+  'luvstep1' : ['Luvstep 1', '14 February 2010', '24 Tracks / 3 Quotes', '1:00:20', ''],
+  'luvstep1radio' : ['Luvstep 1, Radio', '14 February 2010', '24 Tracks / 3 Quotes', '1:05:06', 'Mad Decent Worldwide Radio #57 - Luvstep'],
+  'luvstep1live' : ['Luvstep 1, Live', '31 July 2010', '13 Tracks / 2 Quotes', '0:32:14', 'Live at the Philly Mad Decent Block Party, 31 July 2010'],
+  'luvstep2' : ['Luvstep 2', '14 February 2011', '31 Tracks / 3 Quotes', '1:16:00', ''],
+  'luvstep25' : ['Luvstep 2.5', '5 August 2011', '27 Tracks / 3 Quotes', '1:04:07', 'Mad Decent Block Party Set, Director\'s Cut'],
+  'luvstep3' : ['Luvstep 3', '14 February 2012', '30 Tracks / 3 Quotes', '1:18:14', ''],
+  'luvstep4' : ['Luvstep 4', '14 February 2013', '30 Tracks / 3 Quotes', '1:17:35', ''],
+  'luvstep5' : ['Luvstep 5', '15 February 2014 / 21 February 2014', '2 Sets / 51 Tracks / 3 Quotes', '2:08:55 (1:00:00 / 1:09:12)', ''],
+  'luvstep6' : ['Luvstep 6', '14 February 2015', '31 Tracks / 4 Quotes', '1:20:00', ''],
+  'luvstep7' : ['Luvstep 7', '29 February 2016', '39 Tracks / 3 Quotes', '1:37:57', ''],
+  'luvstep8' : ['Luvstep 8', '20 March 2017', '28 Tracks / 3 Quotes', '1:08:10', ''],
+  'luvstep20200428' : ['Luvstep 2020', '28 April 2020', '26 Tracks', '0:56:05', ''],
+  'luvstep20210214' : ['Luvstep 2021', '14 February 2021', '92 Tracks', '4:05:16', '']
+}
