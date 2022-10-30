8f1165fc3c7d65f95af19e1c56fea1f1df578106
qmlearning <116808999+qmlearning@users.noreply.github.com>
2022-10-30 19:23:43 +1030

    Create luvstep1live.js

---
+++ data/luvstep1live.js
@@ -0,0 +1,17 @@
+var luvstep1live = {
+  track1 : ["Katarina", ""],
+  track2 : ["Sky Ferreira, BAR9", "One (BAR9 Remix)"],
+  track3 : ["NERO", "Innocence"],
+  track4 : ["deadmau5, Kaskade", "I Remember (Caspa Remix)"],
+  track5 : ["Rusko", "Hold On feat. Amber Coffman"],
+  track6 : ["Magnetic Man, Angela Hunte", "I Need Air feat. Angela Hunte"],
+  track7 : ["Katy B", "Katy on a Mission"],
+  track8 : ["Professor Green, Lily Allen", "Just Be Good To Green (Joker Remix, Fluff's Greenless Dub)"],
+  track9 : ["M83", "We Own The Sky (Udachi Remix)"],
+  track10 : ["Kavsrave", "PClart"],
+  track11 : ["Bobby Caldwell", "What You Won't Do For Love (DZ Remix)"],
+  track12 : ["Guido", "Mad Sax"],
+  track13 : ["The Boogaloo Crew", "Days Go By"],
+  track14 : ["The Living Graham Bond, Fiona Bevan", "Winter Hunter feat. Fiona Bevan (Bare Noize Remix)"],
+  track15 : ["Don Juan", ""]
+}
