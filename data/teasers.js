28a0f21051f0b81aef453b373e84c848e5ac3282
qmlearning <116808999+qmlearning@users.noreply.github.com>
2022-11-05 11:42:04 +1030

    Create teasers.js

---
+++ data/teasers.js
@@ -0,0 +1,35 @@
+var teasers = {
+  luvstep1 : ["Luvstep 1 Teaser", "11 January 2010", "deadmau5 & Kaskade - I Remember (Caspa Remix)", "Say Anything... (1989)", "https://www.imdb.com/title/tt0098258"],
+  luvstep2 : ["Luvstep 2 Teaser", "22 December 2010", "Magnetic Man - Crossover ft. Katy B", "Garden State (2004)", "https://www.imdb.com/title/tt0333766"],
+  luvstep3 : ["Luvstep 3 Teaser", "14 December 2011", "Blackmill - Let It Be ft. Veela", "Ghost (1990)", "https://www.imdb.com/title/tt0099653"],
+  luvstep4 : ["Luvstep 4 Teaser", "20 December 2012", "M83 - Steve McQueen (BeatauCue Remix)", "Moonrise Kingdom (2012)", "https://www.imdb.com/title/tt1748122"],
+  luvstep5 : ["Luvstep 5 Teaser", "30 January 2014", "iamamiwhoami - fountain", "Before Sunrise (1995)", "https://www.imdb.com/title/tt0112471"],
+  luvstep6 : ["Luvstep 6 Teaser", "9 February 2015", "The Glitch Mob - Beauty Of The Unhidden Heart ft. Sister Crayon", "Guardians of the Galaxy (2014)", "https://www.imdb.com/title/tt2015381"],
+  luvstep7 : ["Luvstep 7 Teaser", "10 February 2016", "Maribou State - Colours In Sea", "A Girl Walks Home Alone at Night (2014)", "https://www.imdb.com/title/tt2326554"],
+  luvstep8 : ["Luvstep 8 Teaser", "10 March 2017", "Hotel Garuda - Fixed On You ft. Violet Days (Aire Atlantica Remix)", "Moonlight (2016)", "https://www.imdb.com/title/tt4975722"],
+  tour2012 : ["Luvstep Tour Recap", "", "Hellfire Machina feat. Infuze - Good Love <br>Kaskade feat. Mindy Gledhill - Eyes (Alvin Risk Remix) <br>Usher - Climax (prod. Diplo)", "", ""]
+}
+
+var teasers_youtube_embed = {
+  luvstep1 : ["https://www.youtube.com/embed/PdYFDZuSFBA", 560, 315],
+  luvstep2 : ["https://www.youtube.com/embed/iwAzyc5vbfE", 560, 315],
+  luvstep3 : ["https://www.youtube.com/embed/_qfl97OINno", 560, 315],
+  luvstep4 : ["https://www.youtube.com/embed/msZh6XF0vEU", 560, 315],
+  luvstep5 : ["https://www.youtube.com/embed/sCEkawL47ms", 560, 315],
+  luvstep6 : ["https://www.youtube.com/embed/wbrBFCiFK3g", 560, 315],
+  luvstep7 : ["https://www.youtube.com/embed/z77b6Qkniqg", 560, 315],
+  luvstep8 : ["https://www.youtube.com/embed/cfAPHfazWog", 560, 315],
+  tour2012 : ["https://www.youtube.com/embed/C-p_VwFToZw", 560, 315]
+}
+
+var teasers_youtube_watch = {
+  luvstep1 : "https://www.youtube.com/watch?v=iwAzyc5vbfE",
+  luvstep2 : "https://www.youtube.com/watch?v=PdYFDZuSFBA",
+  luvstep3 : "https://www.youtube.com/watch?v=_qfl97OINno",
+  luvstep4 : "https://www.youtube.com/watch?v=msZh6XF0vEU",
+  luvstep5 : "https://www.youtube.com/watch?v=sCEkawL47ms",
+  luvstep6 : "https://www.youtube.com/watch?v=wbrBFCiFK3g",
+  luvstep7 : "https://www.youtube.com/watch?v=z77b6Qkniqg",
+  luvstep8 : "https://www.youtube.com/watch?v=cfAPHfazWog",
+  tour2012 : "https://www.youtube.com/watch?v=C-p_VwFToZw"
+}
